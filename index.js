86ff5df7f05bb3991b19aa894c4b96e0ed12bfee
Juan Carlos Ardila <jeoanan@gmail.com>
2022-04-05 15:01:59 -0500

    Se realizan ajustes para enviar correos y para mantener las url seguras

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import conectarDB from './config/db.js';
 import dotenv from "dotenv";
+import cors from 'cors';
 import veterinarioRouters from './routes/veterinarioRoutes.js';
 import pacienteRouters from './routes/pacienteRoutes.js';
 
@@ -10,8 +11,22 @@ dotenv.config();
 
 conectarDB();
 
+const dominiosPermitidos =[process.env.FRONTEND_URL];
+
+const corsOptions ={
+    origin: function(origin,callback){
+        if (dominiosPermitidos.indexOf(origin) !== -1){
+            //El origen del request está permitido
+            callback(null, true)
+        }else{
+            callback(new Error('No permitido por CORS'))
+        }
+    }
+}
+
 const port = process.env.PORT || 4000;
 
+app.use(cors(corsOptions))
 app.use('/api/veterinarios',veterinarioRouters);
 app.use('/api/pacientes',pacienteRouters);
 
